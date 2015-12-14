382a847bbc4b483829e3ca3e8fb4169cc6649b92
Ethan Hann <ethanhann@gmail.com>
2015-12-13 19:55:19 -0500

    Fix names and paths in menu item content directive

--- src/menu-item-content/menu-item-content-directive.js
+++ src/menu-item-content/menu-item-content-directive.js
@@ -2,16 +2,16 @@
 angular.module('eehMenuBs3').directive('eehMenuBs3MenuItemContent', MenuItemContentDirective);
 
 /** @ngInject */
-function MenuItemContentDirective(eehNavigation) {
+function MenuItemContentDirective(eehMenu) {
     return {
         restrict: 'A',
         scope: {
             menuItem: '=eehMenuBs3MenuItemContent'
         },
-        templateUrl: 'template/eeh-menu/menu-item-content/menu-item-content.html',
+        templateUrl: 'template/eeh-menu-bs3/menu-item-content/menu-item-content.html',
         link: function (scope) {
             scope.iconBaseClass = function () {
-                return eehNavigation.iconBaseClass();
+                return eehMenu.iconBaseClass();
             };
         }
     };
